ae7cffddd18bb7ddfd8f474ac744459f4c65bdd5
Eric White <110315163+ericwhite710@users.noreply.github.com>
2023-01-21 11:43:29 -0500

    Add files via upload

---
+++ Joins.sql
@@ -0,0 +1,35 @@
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees w/ emp_no
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables w/ aliase
+SELECT 
+	 d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+DROP TABLE current_emp;
+
+-- Joining retirement_info and dept_emp tables w/ aliase
+SELECT 
+	ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+SELECT * FROM current_emp
