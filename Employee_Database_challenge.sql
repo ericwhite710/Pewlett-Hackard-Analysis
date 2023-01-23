ecf2eac3fc7033ef99d6ba206d5c970ccc95bebc
Eric White <110315163+ericwhite710@users.noreply.github.com>
2023-01-23 03:47:54 -0500

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,42 +1,84 @@
--- D1
--- Retrieve emp_no, first_name, last_name columns from Employees table.
-select emp_no, first_name, last_name
-from employees;
-select count(*)
-from employees;
-
--- Retrieve title, from_date, to_date columns from Titles table.
-select emp_no, title, from_date,to_date from titles;
-select count(*) from titles;
-select e.emp_no, e.first_name, e.last_name,
-       t.title, t.from_date, t.to_date
-into retirement_titles	   
-from employees e 
-inner join titles t on (e.emp_no=t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-order by e.emp_no
-select count(distinct e.emp_no)
-from employees e 
-inner join titles t on (e.emp_no=t.emp_no)
-and t.to_date='9999-01-01'
-select count(*) from retirement_titles;
-
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+-- Retirement Titles D1------------------
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO ret_titles
+FROM employees AS ce
+INNER JOIN titles AS ti
+ON (ce.emp_no = ti.emp_no)
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY ce.emp_no;
+SELECT * FROM ret_titles
+-- Unique Titles D1-----------------------
+SELECT DISTINCT ON (rt.emp_no) 
+	rt.emp_no,
+	rt.first_name, 
+	rt.last_name, 
+	rt.title
 INTO unique_titles
-FROM retirement_titles
-WHERE to_date='9999-01-01'
-ORDER BY emp_no, to_date DESC;
-select count(*) from unique_titles;
+FROM ret_titles as rt
+WHERE rt.to_date = '9999-01-01'
+ORDER BY rt.emp_no ASC, rt.to_date DESC;
+SELECT * FROM unique_titles
+-- Retiring Titles D1----------------------
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY COUNT(ut.title) DESC;
+SELECT * FROM retiring_titles
+-- Mentorship Eligibilty D2----------------
+SELECT DISTINCT ON (e.emp_no) 
+		e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC, de.from_date DESC;
+SELECT * FROM mentorship_eligibilty
 
--- Write another query in the Employee_Database_challenge.sql file to retrieve the number of employees by their most recent job title who are about to retire.
-select count(*) as count, title
-into retiring_titles
-from unique_titles
-group by title
-order by count(*) desc;
 
-select * from retiring_titles;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 3: Report on the employee database analysis
+
+
