c5aa4ea2e9a812235fdbdf5af8fe282802602a87
Eric White <110315163+ericwhite710@users.noreply.github.com>
2023-01-23 10:59:22 -0500

    Add files via upload

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,4 +1,4 @@
--- Retirement Titles D1------------------
+-- Retirement Titles D1--------------------------------
 SELECT ce.emp_no,
 	ce.first_name,
 	ce.last_name,
@@ -12,7 +12,7 @@ ON (ce.emp_no = ti.emp_no)
 WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY ce.emp_no;
 SELECT * FROM ret_titles
--- Unique Titles D1-----------------------
+-- Unique Titles D1------------------------------------
 SELECT DISTINCT ON (rt.emp_no) 
 	rt.emp_no,
 	rt.first_name, 
@@ -23,14 +23,14 @@ FROM ret_titles as rt
 WHERE rt.to_date = '9999-01-01'
 ORDER BY rt.emp_no ASC, rt.to_date DESC;
 SELECT * FROM unique_titles
--- Retiring Titles D1----------------------
+-- Retiring Titles D1----------------------------------
 SELECT COUNT(ut.title), ut.title
 INTO retiring_titles
 FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
 SELECT * FROM retiring_titles
--- Mentorship Eligibilty D2----------------
+-- Mentorship Eligibilty D2----------------------------
 SELECT DISTINCT ON (e.emp_no) 
 		e.emp_no,
 		e.first_name,
@@ -49,14 +49,13 @@ WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 AND (de.to_date = '9999-01-01')
 ORDER BY e.emp_no ASC, de.from_date DESC;
 SELECT * FROM mentorship_eligibilty
-
-
-
-
-
-
-
-
+-- mentorship_eligibilty_count D3-----------------------
+SELECT COUNT (me.emp_no), me.title
+INTO mentorship_eligibilty_count 
+FROM mentorship_eligibilty as me
+GROUP BY me.title
+ORDER BY COUNT(me.title) DESC;
+SELECT * FROM mentorship_eligibilty_count 
 
 
 
